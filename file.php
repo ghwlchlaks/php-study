f3774a4c6e317bce1a6ce7d38ed750737d284d74
ghwlchlaks <ghwlchlaks@naver.com>
2019-06-06 23:48:17 +0900

    파일 쓰기 예제 , file.php

--- file.php
+++ file.php
@@ -24,4 +24,25 @@
         if(!feof($fp))
             echo $char;
     }
+
+
+    // 파일 쓰기 예제
+
+    $name = $_POST["name"];          // 전송받은 데이터 대입
+    $gender = $_POST["gender"];
+    $email = $_POST["email"];
+
+
+    echo "이름 : ".$name."<br/>";    // 데이터 출력
+    echo "성별 : ".$gender."<br/>";
+    echo "이메일 : ".$email;
+
+    $fp = fopen("list.txt", 'a');    // list.txt 파일을 쓰기 전용으로 열고 반환된 파일 포인터를 $fp에 저장.
+
+    $str = $name."\t".$gender."\t".$email."\n";
+    fwrite($fp, $str);               // list.txt 파일에 $str 변수를 저장함.
+
+    fclose($fp);                     // list.txt 파일 닫음.
+
+    echo "회원 등록 완료";
 ?>
